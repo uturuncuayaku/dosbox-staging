4f136706340c2afed797436ca87b0e373200cbb4
Andres T <andres.trujillo@snhu.edu>
2026-07-23 23:49:28 -0500

    Hook DOS_OpenFile to search APPEND directories

--- src/dos/dos_files.cpp
+++ src/dos/dos_files.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText:  2020-2026 The DOSBox Staging Team
+// SPDX-FileCopyrightText:  2020-2026 Antigravity
 // SPDX-FileCopyrightText:  2002-2021 The DOSBox Team
 // SPDX-License-Identifier: GPL-2.0-or-later
 
