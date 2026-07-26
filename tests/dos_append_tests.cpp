7b99b6830dff0c716062121de9626ba995230e92
Andres T <andres.trujillo@snhu.edu>
2026-07-26 00:32:11 -0500

    Isolate test drive mount to test_scratch directory in dos_append_tests

--- tests/dos_append_tests.cpp
+++ tests/dos_append_tests.cpp
@@ -4,6 +4,7 @@
 
 #include "dos/dos_append.h"
 
+#include <filesystem>
 #include <string>
 
 #include <gtest/gtest.h>
@@ -29,8 +30,9 @@ protected:
 	{
 		DOSBoxTestFixture::SetUp();
 
-		LOG_MSG("Mount C drive to current directory so DOS_MakeDir works");
-		Drives[2] = std::make_shared<localDrive>(".", 512, 1, 1, 1, 1, false);
+		std::filesystem::create_directories("test_scratch");
+		LOG_MSG("Mount C drive to isolated test_scratch directory so DOS_MakeDir works");
+		Drives[2] = std::make_shared<localDrive>("test_scratch/", 512, 1, 1, 1, 1, false);
 		DOS_SetDefaultDrive(2);
 
 		LOG_MSG("Ensure APPEND is clear before each test");
@@ -43,6 +45,10 @@ protected:
 		LOG_MSG("Resetting the drives list");
 		Drives[2].reset();
 		dos_append::SetDirectories("");
+
+		std::error_code ec;
+		std::filesystem::remove_all("test_scratch", ec);
+
 		LOG_MSG("Resetting the dos test fixture");
 		DOSBoxTestFixture::TearDown();
 	}
